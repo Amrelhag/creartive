da53be106ef8ce05086844e93ec67dcfd5696fa7
amr ali <amrelhaj10@gmail.com>
2025-03-21 08:11:54 +0200

    +change popular images

--- lib/core/reusable_component/AssetsManager.dart
+++ lib/core/reusable_component/AssetsManager.dart
@@ -20,4 +20,13 @@ static const String Cat_woman="assets/images/Cat&woman.jpg";
 static const String dog="assets/images/dog.jpg";
 static const String lighthouse="assets/images/lighthouse.jpg";
 static const String redhands="assets/images/redhands.jpg";
+
+
+//popular arts images
+static const String Parabman="assets/images/Parabman.jpg";
+static const String PBgirl="assets/images/PBgirl.jpg";
+static const String PFlowersMan="assets/images/PFlowersMan.jpg";
+static const String PGGirl="assets/images/PGGirl.jpg";
+static const String Pgirl="assets/images/Pgirl.jpg";
+static const String Pman="assets/images/Pman.jpg";
 }
--- lib/ui/Home/widget/tabBar_widgets/popular_Arts_widget.dart
+++ lib/ui/Home/widget/tabBar_widgets/popular_Arts_widget.dart
@@ -2,7 +2,8 @@ import 'package:creartive/core/reusable_component/AssetsManager.dart';
 import 'package:flutter/material.dart';
 
 class PopularArtsWidget extends StatelessWidget {
-  const PopularArtsWidget({super.key});
+  String Pimage;
+   PopularArtsWidget({super.key,required this.Pimage});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,7 @@ class PopularArtsWidget extends StatelessWidget {
           width: 100,
           height: 100,
           child: Image.asset(
-            AssetsManager.fixedImage,
+            Pimage,
             fit: BoxFit.cover,
           ),
         ),
